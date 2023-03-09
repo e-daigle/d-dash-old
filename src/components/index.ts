469289f511006f1cb19de75e7ef85c0e1c57e782
emile-daigle <emiledaigledev@gmail.com>
2023-03-08 20:46:41 -0500

    exports 1.1.1

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export { default as DDContainer } from "./DDashContainer";
-export { default as SmallCard} from "./DDashSmallCard"
-export { default as SmallSection} from "./DDashSmallSection"
-export { default as LargeSection} from "./DDashLargeSection"
-export { default as DDashSideMenu} from "./DDashSideMenu"
-export { default as DDashTable} from "./DataTable/DDashTable"
+export { default as DDashContainer } from "./DDashContainer";
+export { default as DDashSmallCard } from "./DDashSmallCard";
+export { default as DDashSmallSection } from "./DDashSmallSection";
+export { default as DDashLargeSection } from "./DDashLargeSection";
+export { default as DDashSideMenu } from "./DDashSideMenu";
+export { default as DDashTable } from "./DataTable/DDashTable";
